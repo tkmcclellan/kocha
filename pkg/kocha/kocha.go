7e22b0aa9ba3efd0b3811acb1572c4b0e110ae5b
Tyler McClellan <tkmcclellan@pop-os.localdomain>
2021-08-21 13:31:43 -0500

    feat: add db initialization

--- pkg/kocha/kocha.go
+++ pkg/kocha/kocha.go
@@ -1,7 +1,42 @@
 package kocha
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+	"os"
+	"path"
 
-func Test() {
-	fmt.Println("Hello World")
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func Init() {
+	homedir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dirpath := path.Join(homedir, ".kocha")
+
+	_, err = os.Stat(dirpath)
+	if os.IsNotExist(err) {
+		os.Mkdir(dirpath, 0775)
+	}
+	dbpath := path.Join(dirpath, "manga.db")
+
+	db, err := sql.Open("sqlite3", dbpath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	createTables := ``
+
+	_, err = db.Exec(createTables)
+
+	if err != nil {
+		log.Printf("%q: %s\n", err, createTables)
+	}
+}
+
+func Add() {
+	Init()
 }
